dd4d63fca0d8a56d25b7bb06a19494a0f4239053
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-02 21:27:33 -0600

    Created alieninvasion class and __init__ def

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,9 @@
 import sys 
 import pygame
 
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
