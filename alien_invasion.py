4b0e66274cb5c52f19788d36572fcd649c38daa4
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-02 21:34:12 -0600

    created run_game module and quit path

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,4 +10,11 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
-        
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.game.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
