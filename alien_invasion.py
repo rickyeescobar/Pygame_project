5e838accf417df615203cb59dd519084e35cf932
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-02 21:29:46 -0600

    wrote screen method and set game caption

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,3 +7,7 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+        
