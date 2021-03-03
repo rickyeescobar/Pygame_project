07c16572a5733ea1af19d00ad228d9aa42d50647
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-02 21:36:47 -0600

    make a game instance, and run the game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,3 +21,8 @@ class AlienInvasion:
 
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
+
+if __name__ == '__main__':
+    # make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
