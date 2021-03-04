d9152e7aed96a26bdd2023a2189d32bdd7011aa4
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-03 22:15:26 -0600

    finished part of ship

--- ship.py
+++ ship.py
@@ -8,6 +8,19 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Load the ship image and get its rect. 
+        # Load the ship image and get its rect.      ######when workng with ships edge, use center, center x, centery
+        ## when you're working with the edge of screen use, top, bottom, left, or right.
+        # note = (0,0) is at the top left of screen. origin is at top left.
+
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    
+
