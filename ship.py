9f904f4d6e9b90d91a23e8b85e51d6a863988b29
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-04 00:16:00 -0600

    added float to the coordinates, changed rect to x

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.      ######when workng with ships edge, use center, center x, centery
@@ -18,6 +19,9 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #store a decimal value for the ship's horizontal position. 
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
@@ -25,10 +29,14 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on movement flags."""
+        # Upate the ship's x value, not the rect. 
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # update rect object from self.x 
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
