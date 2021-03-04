fef80a6b02cd6865e898c848c006ae58eb55a90d
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-03 23:52:56 -0600

    added self.ship.update to rungame

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
  
 
