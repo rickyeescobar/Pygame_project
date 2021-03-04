9d4fe642b2445d7ce8f2d3394d7de871dba5a413
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-03 21:20:12 -0600

    fixed gitignore

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,7 +25,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.game.get():
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
             #^an event is an action that the user performs while playing the game, such as pressing a key or moving
