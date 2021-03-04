2f98b507a9fdf0f408d1189384a907c3ae1d4038
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-04 00:11:16 -0600

    changed settings speed to 1.5

--- settings.py
+++ settings.py
@@ -7,7 +7,8 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (230,230,230)
-
-        
         #set the background color. 
         #color goes from 0-255 (red, green, blue)
+
+        # Ship settings
+        self.ship_speed = 1.5
