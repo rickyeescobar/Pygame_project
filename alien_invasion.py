917b117880a841bad687b36ae41f644d79a52a80
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-05 14:11:35 -0600

    adding rows

--- alien_invasion.py
+++ alien_invasion.py
@@ -95,21 +95,29 @@ class AlienInvasion:
         # Create an alien and find the number of aliens in a row. 
         # Spacing between each alien is equal to one alien width.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.width
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Creeate the first row of aliens. 
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        # Determine the number of rows of aliens that fit on the screen. 
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - 
+        (3 * alien_height) - ship_height)
+        number_rows = available_space_y // 2 * alien_height
+
+        # Create the full fleet of aliens.
+        for row_number in range(number_rows): 
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
 
     
-    def _create_alien(self, alien_number):                             # we set up a loop to count aliens we need to make
-        """Create an alien and place it in the row."""                 #we create new alien, set its x coords. to place in row
-        alien = Alien(self)                                            # each alien pushed right one alien width from left margin
-        alien_width = alien.rect.width                                 #muliply aline width by 2 for space between, multiply by position
-        alien.x = alien_width + 2 * alien_width * alien_number         # use aliens x attr. to set position of its rect
-        alien.rect.x = alien.x                                         #add each new alien to the group aliens
+    def _create_alien(self, alien_number, row_number):                          # we set up a loop to count aliens we need to make
+        """Create an alien and place it in the row."""                          #we create new alien, set its x coords. to place in row
+        alien = Alien(self)                                                     # each alien pushed right one alien width from left margin
+        alien_width, alien_height = alien.rect.size                             #muliply aline width by 2 for space between, multiply by position
+        alien.x = alien_width + 2 * alien_width * alien_number                  # use aliens x attr. to set position of its rect
+        alien.rect.x = alien.x                                                  #add each new alien to the group aliens
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number     
         self.aliens.add(alien) 
 
 
