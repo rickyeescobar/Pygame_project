9b549f041453617a2f85da2896b25c13a58e4f7c
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-05 12:39:00 -0600

    typo in alien.py

--- alien.py
+++ alien.py
@@ -4,7 +4,7 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
 
-    def __init__ (self,ai_agme):
+    def __init__ (self,ai_game):
         """Initialize the alien and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
