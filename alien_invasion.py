415160f79105c3dbb744b09afb61ae9466b7f275
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-06 17:47:02 -0600

    making the fleet move 266

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,6 +37,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
@@ -88,6 +89,10 @@ class AlienInvasion:
             if bullet in self.bullets.copy():                  # of the loop
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
 
     def _create_fleet(self):
