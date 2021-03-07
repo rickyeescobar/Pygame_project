275d133d4d59d897ed6938124dd4062dde63291e
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-07 13:38:31 -0600

    finished game 300

--- alien_invasion.py
+++ alien_invasion.py
@@ -86,6 +86,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             # get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -224,8 +225,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets. 
             self.aliens.empty()
