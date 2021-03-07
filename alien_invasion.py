ba10711eda267a7ef4d20dd4590279e4ec4cbe40
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-06 19:23:47 -0600

    Responding to alien and ship collisions 275

--- alien_invasion.py
+++ alien_invasion.py
@@ -121,7 +121,7 @@ class AlienInvasion:
 
         # Look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
-            print("Ship hit!!!")
+            self._ship_hit()
 
 
     def _create_fleet(self):
@@ -166,6 +166,22 @@ class AlienInvasion:
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+        # Decrement ships_left.
+        self.stats.ships_left -= 1
+
+        # Get rid of any remaining aliens and bullets. 
+        self.aliens.empty()
+        self.bullets.empty()
+
+        # Create a new fleet and center the ship. 
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # pause. 
+        sleep(0.5)
 
 
     def _update_screen(self):
