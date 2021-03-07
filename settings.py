9db4829fd8f2817e71992db3288b55e958a89072
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-06 18:17:38 -0600

    speeding up the bullets

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet settings 
-        self.bullet_speed = 1.0
+        self.bullet_speed = 10      #default = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
@@ -26,7 +26,7 @@ class Settings:
         # fleet_direction of 1 represents right; -1 represents left. 
         self.fleet_direction = 1
 
-        
+
 
 
 
