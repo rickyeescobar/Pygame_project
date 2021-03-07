c06aafcc1ccfe428c1cf772213fd574123273b64
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-06 20:48:02 -0600

    resetting the game 286

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
 
     def __init__ (self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         # Screen settings 
         self.screen_width = 1200
         self.screen_height = 600
@@ -11,24 +11,27 @@ class Settings:
         #color goes from 0-255 (red, green, blue)
 
         # Ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Bullet settings 
-        self.bullet_speed = 10      #default = 1.5
         self.bullet_width = 30      # default = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 30      #default = 3
 
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 50      #default =10
+
+        # How quickly the game speeds up 
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 10      #default = 3
+        self.alien_speed = 1.0
+
         # fleet_direction of 1 represents right; -1 represents left. 
         self.fleet_direction = 1
-
-
-
-
-
-        
