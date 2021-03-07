6b8a6b7c22889af332ea6aa865e219391a93b2ac
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-06 18:19:31 -0600

    messed with settings

--- settings.py
+++ settings.py
@@ -15,10 +15,10 @@ class Settings:
 
         # Bullet settings 
         self.bullet_speed = 10      #default = 1.5
-        self.bullet_width = 3
+        self.bullet_width = 300      # default = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 30      #default = 3
 
         # Alien settings
         self.alien_speed = 1.0
