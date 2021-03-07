3a4d1f5e28fe55e0f26792033722f9ae389b9c53
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-06 20:40:07 -0600

    hiding the mouse cursor

--- alien_invasion.py
+++ alien_invasion.py
@@ -220,6 +220,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False 
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
