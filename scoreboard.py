d327870e5d6c44c09a446e4cb7a339da033714d3
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-06 21:20:10 -0600

    debugged

--- scoreboard.py
+++ scoreboard.py
@@ -25,11 +25,10 @@ class Scoreboard:
 
         # Display the score at the top right of the screen.
         self.score_rect = self.score_image.get_rect()
-        self.score_rect.right = self.screen_rec.right - 20
+        self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
     def show_score(self):
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
 
-        
