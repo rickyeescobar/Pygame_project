bba091443dcce1d99afcd3ddea8d116df7349e1e
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-06 20:21:58 -0600

    debugged 283

--- button.py
+++ button.py
@@ -14,7 +14,7 @@ class Button:
         self.font = pygame.font.SysFont(None, 48)
 
         # Build the button's rect object and center it.
-        self.rect = pygmae.Rect(0, 0, self.width, self.height)
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
 
         # The button message needs to be prepped only once.
