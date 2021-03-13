93917b5581340ade897e01c728236e30afb6d5bd
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-03-12 21:32:17 -0600

    faster

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
         self.bullet_speed = 8      #default = 3
-        self.alien_speed = 1.0
+        self.alien_speed = 1.5   #default 1
 
         # fleet_direction of 1 represents right; -1 represents left. 
         self.fleet_direction = 1
